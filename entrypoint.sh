3d2dfa21894cd7017f5e03caa98b29f3beac80de
red3aaa <m15013049151@gmail.com>
2025-05-10 17:00:29 +0800

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+umask ${UMASK}
+
+if [ "$1" = "version" ]; then
+  ./alist version
+else
+  if [ "$RUN_ARIA2" = "true" ]; then
+     
+    nohup aria2c \
+      --enable-rpc \
+      --rpc-allow-origin-all \
+      --conf-path=/opt/aria2/.aria2/aria2.conf \
+      >/dev/null 2>&1 &
+  fi
+
+  
+  ./alist server --no-prefix
+fi
