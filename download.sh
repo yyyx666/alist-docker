496c1a12c866c3d341895a7d79cc1df9318c7191
red3aaa <m15013049151@gmail.com>
2025-06-23 23:06:43 +0800

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 if [ -n "$CONFIG_URL" ]; then
     # 使用curl下载
-    curl -fsSL "$CONFIG_URL" -o /opt/alist/data/config.json
+    curl -fsSL "$CONFIG_URL" -o /opt/openlist/data/config.json
     # 或用wget（如果你没有curl）
-    # wget -O /opt/alist/data/config.json "$CONFIG_URL"
+    # wget -O /opt/openlist/data/config.json "$CONFIG_URL"
 else
     echo "环境变量 CONFIG_URL 未设置"
 fi
